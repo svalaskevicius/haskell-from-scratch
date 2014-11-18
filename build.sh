c4728ac345dc4f2e135755ed5a10f530c5db74a9
Sarunas Valaskevicius <rakatan@gmail.com>
2014-11-18 21:51:23 +0000

    build level 2 slides

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-pandoc -f markdown -t revealjs -o haskell_from_scratch.html haskell_from_scratch.md
+pandoc --slide-level=2 -f markdown -t revealjs -o haskell_from_scratch.html haskell_from_scratch.md
 
 php -r '$tpl = file_get_contents("hfs_tpl.html"); $data = file_get_contents("haskell_from_scratch.html"); file_put_contents("haskell.html", str_replace("##SLIDE_CONTENTS##", $data, $tpl));'

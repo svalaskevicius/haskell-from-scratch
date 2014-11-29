52751f2f64b1a6081e37945806cd4b3c926c455e
Sarunas Valaskevicius <rakatan@gmail.com>
2014-11-29 15:36:38 +0000

    add param parsing

--- task/src/Crawly.hs
+++ task/src/Crawly.hs
@@ -1,4 +1,38 @@
 module Main (main) where
 
+import System.Environment (getArgs)
+import System.IO (hPutStrLn, stderr)
+import System.Exit (exitFailure)
+
+data Params = Params {
+    count :: Int,
+    url :: String
+} deriving (Show)
+
+usage :: String
+usage = "<program> <count> <url>\n\
+        \program: path to the executable\n\
+        \count: number of sub-pages to follow\n\
+        \url: the starting url"
+
+parseInt :: String -> Maybe Int
+parseInt = checkError . reads 
+    where checkError [(value, "")] = Just value
+          checkError _ = Nothing
+
+parseParams :: [String] -> Maybe Params
+parseParams args = if (length args /= 2) then Nothing
+                   else do
+                       count <- parseInt $ args!!0
+                       let url = args!!1
+                       Just $ Params count url
+
 main :: IO()
-main = putStrLn "hi"
+main = do
+    args <- getArgs
+    process $ parseParams args
+    where process (Just params) = do
+                                    putStrLn $ show params
+          process Nothing = do
+                                    hPutStrLn stderr usage
+                                    exitFailure

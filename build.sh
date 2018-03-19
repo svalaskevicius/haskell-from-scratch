63a4c8675c4b8322fe33278bb6d041acd1b13f18
Sarunas Valaskevicius <rakatan@gmail.com>
2018-03-19 22:05:08 +0000

    fix quoted list

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 remarker -s haskell_from_scratch.md build
 
-odpdown  --break-master Impress --content-master Impress1 -n haskell_from_scratch.md template.odp  haskell_from_scratch.odp
-
+cat haskell_from_scratch.md | perl -p -e 's/^\.(center|pull-right)\[(.*?)\]$/$2/' > _haskell_from_scratch.md
+odpdown  --break-master Impress --content-master Impress1 -n _haskell_from_scratch.md template.odp  haskell_from_scratch.odp
+rm _haskell_from_scratch.md
 
